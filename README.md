6ef46ee0c01678f4b303d1619f49c9c113aabdee
wb-lp356237 <wb-lp356237@alibaba-inc.com>
2020-07-13 20:38:57 +0800

    eslint 配置修改

--- README.md
+++ README.md
@@ -1,18 +1,31 @@
 # eslint-config-react-lens
-eslint-config-react-lens 是一个可分享 ESLint 配置包，用于React+Typescript或React+Javascript项目
+
+eslint-config-react-lens 是一个可分享 ESLint 配置包，用于 React+Typescript 或 React+Javascript 项目
+
+该配置是在`eslint-config-ali`的基础上添加了一些规则，删除了一些不太友好的规则
 
 ## 用法
+
 1、安装依赖
+
 ```
 npm -D eslint-config-react-lens
+// 安装该配置的一些依赖项：
+"eslint-config-ali": "^10.0.0",
+"eslint-config-prettier": "^6.11.0",
+"eslint-plugin-import": "^2.22.0",
+"eslint-plugin-prettier": "^3.1.4",
+"eslint-plugin-react": "^7.20.3",
+"eslint-plugin-react-hooks": "^4.0.7",
+"@typescript-eslint/eslint-plugin": "^3.6.0",
+"@typescript-eslint/parser": "^3.6.0"
 ```
+
 2、在 .eslintrc 文件中配置
+
 ```
 {
   "extends": "eslint-config-react-lens",
   "rules": {} // 自定义规则，会覆盖eslint-config-react-lens中的规则
 }
 ```
-
-
-
--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 module.exports = {
     extends: [
-      "eslint-config-ali/react",
-      "eslint-config-ali/typescript/react",
+      "eslint-config-ali/react", // 用于校验jsx文件格式
+      "eslint-config-ali/typescript/react", // 用于校验ts文件
       "prettier" // eslint-config-prettier 可以关闭所有不必要的规则或可能与Prettier冲突的规则。要确保其放在extends数组的最后
     ],
     plugins: ["prettier"], // eslint-plugin-prettier插件会调用prettier对你的代码风格进行检查
-    root: true,
+    root: true, // 
     rules: {
       "prettier/prettier": ["error"], // 表示被prettier标记的地方抛出错误信息
-      "import/prefer-default-export": 0,
+      "import/prefer-default-export": 0, // 当模块只输出一个变量时，是否需要添加default
       "@typescript-eslint/indent": 0,
-      "no-nested-ternary": 0,
       "react/jsx-indent": 0,
       "react/prop-types": 0,
       "no-script-url": 0,
@@ -26,7 +25,7 @@ module.exports = {
       semi: 0,
       "@typescript-eslint/no-empty-interface": 0,
       "default-case": 0,
-      "sort-imports": [
+      "sort-imports": [ // import引入顺序
         "error",
         {
           ignoreCase: true, // 为true表示：忽略 import 语句本地名称的大小写
