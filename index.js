8afd0c17915c835ce76491df01f40690dead2b20
wb-lp356237 <wb-lp356237@alibaba-inc.com>
2020-07-14 10:51:55 +0800

    eslint 配置修改

--- index.js
+++ index.js
@@ -5,23 +5,26 @@ module.exports = {
       "prettier" // eslint-config-prettier 可以关闭所有不必要的规则或可能与Prettier冲突的规则。要确保其放在extends数组的最后
     ],
     plugins: ["prettier"], // eslint-plugin-prettier插件会调用prettier对你的代码风格进行检查
-    root: true, // 
+    root: true,
     rules: {
       "prettier/prettier": ["error"], // 表示被prettier标记的地方抛出错误信息
       "import/prefer-default-export": 0, // 当模块只输出一个变量时，是否需要添加default
-      "@typescript-eslint/indent": 0,
-      "react/jsx-indent": 0,
-      "react/prop-types": 0,
-      "react-hooks/exhaustive-deps": 0,
-      eqeqeq: 0,
-      "react/jsx-no-bind": 0,
-      "react/no-array-index-key": 0,
-      "no-throw-literal": 0,
-      "@typescript-eslint/no-require-imports": 0,
-      "react/jsx-indent-props": 0,
-      semi: 0,
-      "@typescript-eslint/no-empty-interface": 0,
-      "default-case": 0,
+      "react/jsx-indent": 0, // jsx缩进
+      "react/jsx-indent-props": 0, // jsx中的props缩进
+      "react/prop-types": 0, // prop 需要 propTypes 验证类型
+      "@typescript-eslint/indent": 0, // tsx 缩进
+      "@typescript-eslint/no-require-imports": 0, // 是否允许require()导入
+      "@typescript-eslint/no-empty-interface": 0, // 是否允许空的interface
+      "default-case": 0, // 默认case
+      'no-magic-numbers': [ // 是否禁用魔术数字
+        'warning',
+        {
+          ignore: [],
+          ignoreArrayIndexes: true, // 忽略数组索引
+          enforceConst: true,
+          detectObjects: false,
+        },
+      ],
       "sort-imports": [ // import引入顺序
         "error",
         {
